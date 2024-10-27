dd12d8cffc4925d4364280982961fbd6e282228a
manoj1995madushanka <madushankamanoj414@gmail.com>
2024-10-27 12:00:09 +0530

    fix cors origin issue

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,6 @@
 const authController = require('../controllers/authController')
 const router = require('express').Router()
 
-router.post('/admin-login',authController.admin_login)
+router.post('/admin_login',authController.admin_login)
 
 module.exports= router
--- server.js
+++ server.js
@@ -4,6 +4,17 @@ const app = express();
 
 require('dotenv').config()
 
+const cors = require('cors')
+const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser')
+
+app.use(cors({
+  origin:['http://localhost:3000'],
+  credentials: true
+}))
+app.use(bodyParser.json())
+app.use(cookieParser)
+
 app.use('/api', require('./routes/authRoutes'))
 
 app.get('/', (req,res)=>{
