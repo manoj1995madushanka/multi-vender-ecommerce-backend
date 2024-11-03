2ee855849668fd4efc8e6c425dda0e1a5243391f
manoj1995madushanka <madushankamanoj414@gmail.com>
2024-11-03 11:52:45 +0530

    handle admin login error

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,10 +1,19 @@
-class AuthController{
-  admin_login = async(req,res)=>{
-    const {email,password} = req.body
-    try{
+const adminModel = require('../models/adminModel')
+const {responseReturn} = require("../utilities/response");
 
-    }catch (e){
+class AuthController {
+  admin_login = async (req, res) => {
+    const {email, password} = req.body
+    try {
+      const admin = await adminModel.findOne({email}).select('+password')
+      console.log(admin)
+      if (admin) {
 
+      } else{
+        responseReturn(res, 404, {error: 'Email not found'})
+      }
+    } catch (e) {
+      responseReturn(res, 500, {error: e.message})
     }
   }
 }
