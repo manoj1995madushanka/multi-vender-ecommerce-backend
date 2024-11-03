ba699ca0c04990ec7eb9c9e9ee355db2377feb18
manoj1995madushanka <madushankamanoj414@gmail.com>
2024-11-03 12:58:59 +0530

    create jwt token and set it to cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,7 @@
 const adminModel = require('../models/adminModel')
 const {responseReturn} = require("../utilities/response");
+const {createToken} = require("../utilities/tokenCreate");
+const bcrpty = require('bcrypt')
 
 class AuthController {
   admin_login = async (req, res) => {
@@ -8,8 +10,22 @@ class AuthController {
       const admin = await adminModel.findOne({email}).select('+password')
       console.log(admin)
       if (admin) {
+        const match = await bcrpty.compare(password, admin.password)
+        console.log(match)
 
-      } else{
+        if (match) {
+          // generate coockie
+          const token = await createToken({
+            id: admin._id,
+            role: admin.role
+          })
+          res.cookie('accessToken', token, {expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000)})
+
+          responseReturn(res, 200, {token, message: "Login Success"})
+        } else {
+          responseReturn(res, 4040, {error: 'Password Wrong'})
+        }
+      } else {
         responseReturn(res, 404, {error: 'Email not found'})
       }
     } catch (e) {
